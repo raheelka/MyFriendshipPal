249a8899e0f1483ad436d399f35256366c2996d7
Raheel Kazi <raheelka@gmail.com>
2015-08-31 21:03:49 -0400

    Add logout button to Settings tab and clean story board

---
+++ FriendshipPal/SettingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewController.swift
+//  FriendshipPal
+//
+//  Created by Raheel Kazi on 8/31/15.
+//  Copyright (c) 2015 Raheel Kazi. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func logoutUser(sender: AnyObject) {
+        if FBSDKAccessToken.currentAccessToken() != nil {
+            FBSDKLoginManager().logOut()
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let loginViewController = self.storyboard!.instantiateViewControllerWithIdentifier("loginViewController") as! ViewController
+            presentViewController(loginViewController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
